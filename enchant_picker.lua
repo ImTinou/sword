6fe35520121853ccc13bd6f233af4825bba677d1
ImTinou <tinoutestuwu@gmail.com>
2026-04-06 01:43:50 +0200

    Update enchant_picker.lua

--- enchant_picker.lua
+++ enchant_picker.lua
@@ -4,7 +4,7 @@ local player       = game:GetService("Players").LocalPlayer
 local remote       = game:GetService("ReplicatedStorage").Paper.Remotes.__remoteevent
 local TweenService = game:GetService("TweenService")
 
-local VERSION     = "0.05"
+local VERSION     = "0.06"
 local SCAN_RATE   = 0.5
 local MATCH_ALL   = true
 local scanning    = false
@@ -288,7 +288,14 @@ local function startScan(lbl)
     -- Event-based: reagit instantanement quand une sword apparait
     if childAddedConn then childAddedConn:Disconnect() end
     childAddedConn = workspace.Swords.ChildAdded:Connect(function(sword)
-        task.wait(0.05) -- laisse le temps aux enchants de se charger
+        -- Attendre que les enchants soient charges (retry max 2s)
+        local tries = 0
+        while tries < 20 do
+            task.wait(0.1)
+            tries = tries + 1
+            if not sword.Parent then return end  -- sword disparue entre temps
+            if #getSwordEnchants(sword) > 0 then break end
+        end
         handleSword(sword, lbl)
     end)
 
